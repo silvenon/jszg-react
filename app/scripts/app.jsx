9504b1031210d9999379eaa07faafedeb653bcb5
Matija Marohnić <matija.marohnic@gmail.com>
2015-09-22 20:04:18 +0200

    :fire: :fire:

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -1,69 +1,15 @@
 import React from 'react/addons';
-import Router from 'react-router';
-import fetch from './helpers/fetch.js';
-import Icon from './components/icon';
-import Loader from './components/loader';
 
-import 'svg4everybody';
-import './fonts.js';
-
-const {
-  Route,
-  RouteHandler
-} = Router;
-
-const App = React.createClass({
+let App = React.createClass({
   propTypes: {},
 
-  getInitialState() {
-    return {
-      loading: true
-    };
-  },
-
-  componentDidMount() {
-    fetch('/index.html').then((res) => {
-      // a little artificial delay never hurt anyone
-      setTimeout(() => {
-        console.log(res);
-        this.setState({
-          loading: false
-        });
-      }, 3000);
-    });
-  },
-
   render() {
-    let content;
-
-    if (this.state.loading) {
-      content = (
-        <div className="container">
-          <Loader />
-        </div>
-      );
-    } else {
-      content = (
-        <div className="container">
-          <h1>{'You look'} <strong>{'great'}</strong> {'today!'}</h1>
-          <Icon symbol="heart" role="presentation" />
-          <RouteHandler />
-        </div>
-      );
-    }
-
-    return content;
+    return (
+      <div className="container">
+        <h1>{'\'Sup'}</h1>
+      </div>
+    );
   }
 });
 
-const routes = (
-  <Route path="/" handler={App}>
-    {/* routes */}
-  </Route>
-);
-
-Router.run(routes, (Handler) => {
-  React.render(<Handler />, document.getElementById('content'));
-});
-
-console.log('Hello World from React!');
+React.render(<App />, document.getElementById('content'));
