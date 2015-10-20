93e20afd903241898cd29f5e25f5ceb0817b1692
Matija Marohnić <matija.marohnic@gmail.com>
2015-10-20 14:18:28 +0200

    Move index.html to the source directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './app',
   output: {
     path: './dist',
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({template: './app/index.html'})
+  ],
   module: {
     loaders: [
       { test: /\.js?$/, loader: 'babel' }
